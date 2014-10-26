848ea7603fd46b78f87e190fa4cbeeabe5d1b948
Mathias Husted <glemdetmakker@live.dk>
2014-10-26 15:29:07 +0100

    Made the installation easier

--- bin/install.rb
+++ bin/install.rb
@@ -1,13 +1,13 @@
 print "Are you sure you want to install the Masic command? [Y/N] "
 confirm_install = gets.chomp.downcase
 if confirm_install == "y"
-	print "Enter the path to masic.rb (leave empty for /home/Masic/src/masic.rb): "
+	print "Enter the path to the Masic folder (leave empty for /home/Masic/): "
 	path = gets.chomp
 	if path == ""
-		path = "/home/Masic/src/masic.rb"
+		path = "/home/Masic/"
 	end
 	masic = File.new("/bin/masic","w")
-	masic.write("ruby #{path}")
+	masic.write("ruby #{path}/src/masic.rb")
 	puts "Done!"
 else
 	puts "Aborting..."

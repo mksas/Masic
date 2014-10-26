c2048e92844698a0761f9a60abbb01f0d3f58942
Mathias Husted <glemdetmakker@live.dk>
2014-10-26 15:22:06 +0100

    Added install script.

---
+++ bin/install.rb
@@ -0,0 +1,15 @@
+print "Are you sure you want to install the Masic command? [Y/N] "
+confirm_install = gets.chomp.downcase
+if confirm_install == "y"
+	print "Enter the path to masic.rb (empty for /home/Masic/src/masic.rb): "
+	path = gets.chomp
+	if path == ""
+		path = "/home/Masic/src/masic.rb"
+	end
+	masic = File.new("/bin/masic","w")
+	masic.write("ruby #{path}")
+	puts "Done!"
+else
+	puts "Aborting..."
+end
+masic.close
